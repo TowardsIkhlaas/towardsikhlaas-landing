de53ef3eaf568a4e81777fa77b366fa2207f1276
ahmd-sh <ahmed.shaikh626@gmail.com>
2024-06-29 20:02:16 +0530

    Update path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     output: "export",
-    basePath: "/towardsikhlaas-landing",
+    assetPrefix: isProd ? 'https://towardsikhlaas.com' : undefined,
     images: {
         unoptimized: true,
     },
